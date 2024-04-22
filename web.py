128980a30599364aa044d3c150d1a58294e94ea3
Janice Ivana <jiva0001@student.monash.edu>
2024-04-22 21:28:47 +1000

    Update web.py

--- web.py
+++ web.py
@@ -3,3 +3,5 @@
 import streamlit as st
 
 st.header("Welcome to FIT3164_DS05 Webpage!")
+
+st.text("Our Retail Product Pricing Optimization and Analytics project involved leveraging the M5 Kaggle dataset to develop optimal pricing strategies. The optimization was studied by analyzing historical sales data, product attributes, and pricing information. By employing data analytics, price elasticity modeling, and visualization techniques, we seek to determine the optimal pricing point for various products and explore the impact of pricing on sales performance.  ")

6d810634bf9524dc87fbe24c7394e4c4bdc0fe87
Janice Ivana <jiva0001@student.monash.edu>
2024-04-22 21:49:25 +1000

    Update 03_Price Optimization.py

--- pages/03_Price Optimization.py
+++ pages/03_Price Optimization.py
@@ -2,6 +2,8 @@
 # Import libraries
 import streamlit as st
 import altair as alt
+import pandas as pd  
+import matplotlib.pylab as plt   
 # import plotly.express as px
 
 #######################
@@ -15,22 +17,68 @@ st.set_page_config(
 alt.themes.enable("dark")
 
 #######################
+# Load data
+calendar = pd.read_csv("calendar.csv")
+h1_df = pd.read_csv('h1_df.csv')
+h2_df= pd.read_csv('h2_df.csv')
+ho1_df = pd.read_csv('ho1_df.csv')
+ho2_df = pd.read_csv('ho2_df.csv')
+f1_df = pd.read_csv('f1_df.csv')
+f2_df = pd.read_csv('f2_df.csv')
+f3_df = pd.read_csv('f3_df.csv')
+
+
+department_data = {
+    'FOODS_3': f3_df,
+    'FOODS_2': f2_df,
+    'FOODS_1' : f1_df,
+    'HOUSEHOLD_2' : ho2_df,
+    'HOUSEHOLD_1': ho1_df, 
+    'HOBBIES_2': h2_df,
+    'HOBBIES_1': h1_df
+
+}
+
+dept_list = [ 'FOODS_3', 'FOODS_2', 'FOODS_1' , 'HOUSEHOLD_2', 'HOUSEHOLD_1', 'HOBBIES_2', 'HOBBIES_1']
+
+
+#######################
 with st.sidebar:
     st.title('💵 P5: Price Optimization')
     
-    # year_list = list(calendar.year.unique())[::-1]
-    # state_list = list(h1_df.state_id.unique())[::-1]
+    year_list = list(calendar.year.unique())[::-1]
+    state_list = list(h1_df.state_id.unique())[::-1]
 
-    # selected_department = st.selectbox('Select a deparment', dept_list)
-    # selected_data = department_data[selected_department]
+    selected_department = st.selectbox('Select a deparment', dept_list)
+    selected_data = department_data[selected_department]
 
-    # # item_list = list(selected_data.item_id.unique())[::-1]
+    selected_state = st.selectbox('Select a state', state_list)
+    selected_data = selected_data[selected_data.state_id == selected_state]
 
-    # # selected_item= st.selectbox('Select an Item', item_list)
-    # # selected_item_level = selected_data[selected_data.item_id == selected_item]
+    selected_year = st.selectbox('Select a year', year_list)
+    selected_data = selected_data[selected_data.year == selected_year]
 
-    # selected_state = st.selectbox('Select a state', state_list)
-    # selected_data = selected_data[selected_data.state_id == selected_state]
+#######################
 
-    # selected_year = st.selectbox('Select a year', year_list)
-    # selected_data = selected_data[selected_data.year == selected_year]
+def vis_elasticity(data):
+    
+    # Create a scatter plot
+    fig, ax = plt.subplots()
+    ax.scatter(data['optimized_price'], data['optimized_revenue'])
+    ax.set_title('Scatter Plot of Price and Revenu')
+    ax.set_xlabel('Price')
+    ax.set_ylabel('Revenue')
+    ax.grid(True)
+
+    # Display the plot using Streamlit
+    st.pyplot(fig)
+
+
+#######################
+# Dashboard Main Panel
+col = st.columns((4, 4), gap='medium')
+
+with col[0]:
+    st.markdown('#### Price Elasticity Model')
+    
+    vis_elasticity(selected_data)

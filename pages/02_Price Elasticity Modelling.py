1a09667375da9367b7eb43b20a1c03e4dafa005c
Janice Ivana <jiva0001@student.monash.edu>
2024-04-22 23:02:10 +1000

    Update 02_Price Elasticity Modelling.py

--- pages/02_Price Elasticity Modelling.py
+++ pages/02_Price Elasticity Modelling.py
@@ -54,10 +54,10 @@ with st.sidebar:
     st.title('📊 P5: Price Elasticity Modelling')
     
     year_list = list(calendar.year.unique())[::-1]
-    year_list = pd.to_datetime(year_list)
+    year_list = pd.to_numeric(year_list)
     year_list.append("Overall")
     st.write(year_list)
-    
+
     state_list = list(h1_df.state_id.unique())[::-1]
 
     selected_department = st.selectbox('Select a deparment', dept_list)

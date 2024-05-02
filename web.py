47a878ff9b67c962c9fc208f014a9f3a43863827
Janice Ivana <jiva0001@student.monash.edu>
2024-05-02 16:51:47 +1000

    Update web.py

--- web.py
+++ web.py
@@ -6,9 +6,9 @@ st.header("Welcome to FIT3164_DS05 Webpage!")
 
 st.write("Our Retail Product Pricing Optimization and Analytics project involved leveraging the M5 Kaggle dataset to develop optimal pricing strategies. The optimization was studied by analyzing historical sales data, product attributes, and pricing information. By employing data analytics, price elasticity modeling, and visualization techniques, we seek to determine the optimal pricing point for various products and explore the impact of pricing on sales performance.  ")
 
-st.write("Meet our team members!")
-st.write("Janice Ivana - 32959796" )
-st.write("Jiayi Wang (Wendy) - 31513638" )
-st.write("Nguyen Pham (Chris) - 30750814" )
-
 st.write("Start of by uploading your data to get your pricing analysis and strategies!")
+
+st.write("Meet our team members!")
+st.write("Janice Ivana - 32959796")
+st.write("Jiayi Wang (Wendy) - 31513638")
+st.write("Nguyen Pham (Chris) - 30750814")

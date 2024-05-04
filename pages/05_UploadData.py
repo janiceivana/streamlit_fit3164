c3454b4017dc5ceb519ba789b3ed0b5ca48a9e8f
npha0026 <npha0026@student.monash.edu>
2024-05-04 19:14:19 +1000

    update st secret

--- pages/05_UploadData.py
+++ pages/05_UploadData.py
@@ -23,7 +23,7 @@ controller.set('user-cred', 'testing')
 
 
 # Create credential for oauth flow
-info1= json.loads( st.session_state['service_acc'])
+info1= json.loads( st.secrets["db_username"])
 
 credentials = service_account.Credentials.from_service_account_info( info = info1
   , scopes=["https://www.googleapis.com/auth/sqlservice.admin"]

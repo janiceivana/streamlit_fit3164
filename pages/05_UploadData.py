0c865fd599bf11efe590463790dd59ad34bf65fd
npha0026 <npha0026@student.monash.edu>
2024-05-09 14:42:57 +1000

    clean up comments

--- pages/05_UploadData.py
+++ pages/05_UploadData.py
@@ -1,19 +1,13 @@
 
 import streamlit as st
 import pandas as pd
-import os
-# Upload file command for use to upload there file
-import sqlalchemy
-from google.oauth2 import service_account
-import google.auth
-
-import json
-from google.cloud.sql.connector import Connector, IPTypes
 import pyodbc
-#from streamlit_cookies_controller import CookieController
+from streamlit_cookies_controller import CookieController
 
+controller = CookieController()
 
-
+# Set a cookie
+controller.set('user-cred', 'testing')
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
@@ -35,64 +29,27 @@ conn = init_connection()
 
 
 
-'''
-controller = CookieController()
-
-# Set a cookie
-controller.set('user-cred', 'testing')
-'''
-
-
-
-
-
-
-
 data_cvs = st.file_uploader("choose a csv file")
 
-'''
+
 if data_cvs is not None:
-    # read file using pandas
+
     df = pd.read_csv(data_cvs.path)
-    # check file format
-    #add file format check here ************ IMPORTANT ***************
 
-    #add quit exit condition if file is not correct
-    with pool.connect() as db_conn:
-
+    with conn.cursor() as cur:
+        seen = []
         for records in df:
+            if records['store_id'] not in seen:
+                seen.append( records['store_id'])
+                insert_stmt1 = 'INSERT INTO STORE (store_id, cookie) VALUES (?, ?)'
+                cur.execute(insert_stmt1, {"store_id": records['store_id'], "cookie": controller.get("user-cred")})
+
+            insert_stmt2= 'INSERT INTO SALE (store_id, price, date,  item_id, dept_id, qty_sold,) VALUES (?,?,?,?,?,?,?)'
+
+    
+
+            cur.execute(insert_stmt2, {"store_id" : records['store_id'] , "cookie" : controller.get("user-cred"), "price" : records['price'] , "date" : records['date'] , "item_id": records["item_id"] ,"dept_id" : records['dept_id'], "qty_sold" : records['qty_sold']})
             
-            
-            insert_stmt1= 'INSERT INTO STORE (store_id, cookie) VALUES (?, ?)'
-    
-    
-            insert_stmt2= 'INSERT INTO SALE (store_id, cookie, price, date,  item_id, dept_id, qty_sold,) VALUES (?,?,?,?,?,?,?)'
-    
-    
-            cnxn.execute(insert_stmt1, {"store_id" : records['store_id'], "cookie" :controller.get("user-cred")})
-            cnxn.execute(insert_stmt2, {"store_id" : records['store_id'] , "cookie" : controller.get("user-cred"), "price" : records['price'] , "date" : records['date'] , "item_id": records["item_id"] ,"dept_id" : records['dept_id'], "qty_sold" : records['qty_sold']})
-            
 
 
 
-
-
-
-
-
-            # inserting file into sqlserver
-            insert_stmt1 = sqlalchemy.text(
-                "INSERT INTO STORE (store_id, cookie) VALUES (:store_id, :cookie)",
-            )
-
-            insert_stmt2 = sqlalchemy.text(
-                "INSERT INTO SALE (store_id, cookie, price, date, qty_sold, item_id, dept_id) VALUES (:store_id, :cookie, :price, :date, :qty_sold, :item_id, :dept_id)",
-            )
-
-
-
-            conn.execute (insert_stmt1, parameters={"store_id" : records['store_id'] , "cookie" : controller.get("user-cred") })
-            conn.execute (insert_stmt2, parameters={"store_id" : records['store_id'] , "cookie" : controller.get("user-cred"), "price" : records['price'] , "date" : records['date'] , "item_id": records["item_id"], "dept_id" : records['dept_id'], "qty_sold" : records['qty_sold']})
-
-            db_conn.commit()
-'''

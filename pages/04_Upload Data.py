758ece9849771f4858c5a82687756865d9625ef4
npha0026 <npha0026@student.monash.edu>
2024-05-10 00:16:12 +1000

    Update 04_Upload Data.py

--- pages/04_Upload Data.py
+++ pages/04_Upload Data.py
@@ -2,6 +2,11 @@
 import streamlit as st
 import pandas as pd
 import pyodbc
+
+
+import streamlit as st
+import pandas as pd
+import pyodbc
 from streamlit_cookies_controller import CookieController
 
 controller = CookieController()
@@ -28,23 +33,23 @@ def init_connection():
 conn = init_connection()
 
 
-
-
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
 
 
     with conn.cursor() as cur:
-        seen = []
+        seen = ['CA_1']
         for i in range(len(df)):
             if df.loc[i,'store_id'] not in seen:
                 seen.append(df.loc[i, 'store_id'])
-                insert_stmt1 = 'INSERT INTO USER (store_id, cookie) VALUES (?, ?)'
-                cur.execute(insert_stmt1,  df.loc[i,'store_id'], controller.get("user-cred"))
+                insert_stmt1 = "INSERT INTO [INFO] (store_id, cookie) VALUES (?, ?)"
+                value1 = (df.loc[i,'store_id'], "testing")
+                cur.execute(insert_stmt1,  value1)
                 conn.commit()
-            insert_stmt2 = 'INSERT INTO RECORD (store_id, price, date,  item_id, dept_id, qty_sold, cost) VALUES (?,?,?,?,?,?,?,?)'
-            cur.execute(insert_stmt2,  df.loc[i,'store_id'], controller.get("user-cred"), df.loc[i,'price'], df.loc[i,'date'] , df.loc[i,'item_id'],df.loc[i,'dept_id'],  df.loc[i,'qty_sold'],df.loc[i,'cost'])
+            insert_stmt2 = "INSERT INTO [RECORD] (store_id, price, date,  item_id, dept_id, qty_sold, cost) VALUES (?,?,?,?,?,?,?)"
+            value2 = (df.loc[i,'store_id'], df.loc[i,'sell_price'], df.loc[i,'date'] , df.loc[i,'item_id'],df.loc[i,'dept_id'],  df.loc[i,'qty_sold'],df.loc[i,'cost_price'])
+            cur.execute(insert_stmt2,  value2)
             conn.commit()
 
 

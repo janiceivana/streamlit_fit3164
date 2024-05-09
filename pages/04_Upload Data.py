7b92ba214762b5c1552c45a1b77ca49a6d6a27b7
npha0026 <npha0026@student.monash.edu>
2024-05-09 22:00:41 +1000

    Update 04_Upload Data.py

--- pages/04_Upload Data.py
+++ pages/04_Upload Data.py
@@ -35,16 +35,16 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
 
 
-with conn.cursor() as cur:
-    seen = []
-    for records in df:
-        if records['store_id'] not in seen:
-            seen.append(records['store_id'])
-            insert_stmt1 = 'INSERT INTO STORE (store_id, cookie) VALUES (?, ?)'
-            cur.execute(insert_stmt1, {"store_id": records['store_id'], "cookie": controller.get("user-cred")})
+    with conn.cursor() as cur:
+        seen = []
+        for records in df:
+            if records['store_id'] not in seen:
+                seen.append(records['store_id'])
+                insert_stmt1 = 'INSERT INTO STORE (store_id, cookie) VALUES (?, ?)'
+                cur.execute(insert_stmt1, {"store_id": records['store_id'], "cookie": controller.get("user-cred")})
+                conn.commit()
+            insert_stmt2 = 'INSERT INTO SALE (store_id, price, date,  item_id, dept_id, qty_sold, cost) VALUES (?,?,?,?,?,?,?,?)'
+            cur.execute(insert_stmt2, {"store_id" : records['store_id'] , "cookie" : controller.get("user-cred"), "price" : records['price'] , "date" : records['date'] , "item_id": records["item_id"] ,"dept_id" : records['dept_id'], "qty_sold" : records['qty_sold'], "cost" : records['cost']})
             conn.commit()
-        insert_stmt2 = 'INSERT INTO SALE (store_id, price, date,  item_id, dept_id, qty_sold, cost) VALUES (?,?,?,?,?,?,?,?)'
-        cur.execute(insert_stmt2, {"store_id" : records['store_id'] , "cookie" : controller.get("user-cred"), "price" : records['price'] , "date" : records['date'] , "item_id": records["item_id"] ,"dept_id" : records['dept_id'], "qty_sold" : records['qty_sold'], "cost" : records['cost']})
-        conn.commit()
 
 

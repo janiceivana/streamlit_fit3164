80b0fb99e11b1e079b3113e18d66bf470971e0aa
Janice Ivana <jiva0001@student.monash.edu>
2024-05-10 14:08:16 +1000

    Update 01_Get Started.py

--- pages/01_Get Started.py
+++ pages/01_Get Started.py
@@ -1,22 +1,22 @@
-import streamlit as st
-import pandas as pd
-from streamlit.state.session_state import get
+# import streamlit as st
+# import pandas as pd
+# # from streamlit.state.session_state import get
 
-def save_dataframes(dataframes):
-    session_state = get(email=None, dataframes={})
-    session_state.dataframes.update(dataframes)
+# def save_dataframes(dataframes):
+#     session_state = get(email=None, dataframes={})
+#     session_state.dataframes.update(dataframes)
 
-def main():
-    st.title("Data Visualization App")
+# def main():
+#     st.title("Data Visualization App")
 
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-    if st.button("Load Data"):
-        dataframes = {}
-        for uploaded_file in uploaded_files:
-            file_name = uploaded_file.name.replace(".csv", "").replace(" ", "_")
-            dataframes[file_name] = pd.read_csv(uploaded_file)
-        save_dataframes(dataframes)
-        st.success("Data loaded successfully!")
+#     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+#     if st.button("Load Data"):
+#         dataframes = {}
+#         for uploaded_file in uploaded_files:
+#             file_name = uploaded_file.name.replace(".csv", "").replace(" ", "_")
+#             dataframes[file_name] = pd.read_csv(uploaded_file)
+#         save_dataframes(dataframes)
+#         st.success("Data loaded successfully!")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()

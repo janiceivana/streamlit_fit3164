6f5c297d249db848a2d29efd85fcbda6d98dae2a
Janice Ivana <jiva0001@student.monash.edu>
2024-05-10 14:05:12 +1000

    Update 01_Get Started.py

--- pages/01_Get Started.py
+++ pages/01_Get Started.py
@@ -1,13 +1,22 @@
 import streamlit as st
 import pandas as pd
+from streamlit.state.session_state import get
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-count = 0
-for uploaded_file in uploaded_files:
-    file_name = uploaded_file.name.replace(".csv", "").replace(" ", "_")
-    exec(f"{file_name} = pd.read_csv(uploaded_file)")
-    st.write("filename:", file_name)
-    count = count + 1
+def save_dataframes(dataframes):
+    session_state = get(email=None, dataframes={})
+    session_state.dataframes.update(dataframes)
 
-    if len(uploaded_files) == count:
-        st.write("Hey, All the files are successfully uploaded")
+def main():
+    st.title("Data Visualization App")
+
+    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    if st.button("Load Data"):
+        dataframes = {}
+        for uploaded_file in uploaded_files:
+            file_name = uploaded_file.name.replace(".csv", "").replace(" ", "_")
+            dataframes[file_name] = pd.read_csv(uploaded_file)
+        save_dataframes(dataframes)
+        st.success("Data loaded successfully!")
+
+if __name__ == "__main__":
+    main()

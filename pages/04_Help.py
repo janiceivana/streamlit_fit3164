434707f935e763e485edad08990c15eb0b86aec5
Janice Ivana <jiva0001@student.monash.edu>
2024-05-11 02:03:39 +1000

    help

--- pages/04_Help.py
+++ pages/04_Help.py
@@ -3,5 +3,6 @@
 import streamlit as st
 
 st.markdown('#### Need Help?')
+st.image("help.jpeg")
 st.write("Please contact our team to help you troubleshoot")
 st.write("Janice: jiva0001@student.monash.edu")

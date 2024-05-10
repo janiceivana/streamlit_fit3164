20e9543543bfbfaf6fe61079a43a3f06ad4dc1f1
Janice Ivana <jiva0001@student.monash.edu>
2024-05-10 14:00:03 +1000

    Update 01_Get Started.py

--- pages/01_Get Started.py
+++ pages/01_Get Started.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pandas as pd
 
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+count = 0
 for uploaded_file in uploaded_files:
     file_name = uploaded_file.name.replace(".csv", "").replace(" ", "_")
     exec(f"{file_name} = pd.read_csv(uploaded_file)")
     st.write("filename:", file_name)
+    count = count + 1
+
+if len(uploaded_files) == count:
+    st.write("Hey, All the files are successfully uploaded")

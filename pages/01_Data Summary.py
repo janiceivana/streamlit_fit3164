631155841dedfdb5a36df60f722aef77817de3db
Janice Ivana <jiva0001@student.monash.edu>
2024-05-11 01:10:42 +1000

    Update 01_Data Summary.py

--- pages/01_Data Summary.py
+++ pages/01_Data Summary.py
@@ -18,6 +18,7 @@ alt.themes.enable("dark")
 
 #######################
 
+st.header("Upload your data here to get your summarisation!")
 st.write("Hi, Welcome to your first step off this application, upload your files here to get your data checked and summarise")
 st.write("Feel free to upload your file more than 1, and make sure is in CSV!")
 st.write("Refer on your left hand side bar to get your elasticity reading and price optimization!")

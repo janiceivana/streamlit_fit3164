6b5c8285cdd2a5cd1a0a108e326bc55b3b233c13
Janice Ivana <jiva0001@student.monash.edu>
2024-05-11 01:03:27 +1000

    Update web.py

--- web.py
+++ web.py
@@ -17,8 +17,7 @@ col1, col2, col3 = st.columns([1,1,1])
 with col1:
     st.write("Janice Ivana - 32959796")
     st.image("IMG-20240108-WA0049.jpg")
-    
-    
+
 with col2:
     st.write("Jiayi Wang (Wendy) - 31513638")
     st.image("Image (1).jpeg")

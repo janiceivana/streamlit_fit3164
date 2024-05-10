52b2116e3dffe7f7422b98ff6c540fdeb2395fcc
Janice Ivana <jiva0001@student.monash.edu>
2024-05-10 13:57:42 +1000

    Update 01_Get Started.py

--- pages/01_Get Started.py
+++ pages/01_Get Started.py
@@ -1,9 +1,8 @@
-
 import streamlit as st
 import pandas as pd
 
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    
+    file_name = uploaded_file.name.replace(".csv", "").replace(" ", "_")
+    exec(f"{file_name} = pd.read_csv(uploaded_file)")
+    st.write("filename:", file_name)

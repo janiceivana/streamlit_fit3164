2b5a5010f3273dd2ab6f95f217b72ed6728ef0fc
Janice Ivana <jiva0001@student.monash.edu>
2024-05-11 01:54:12 +1000

    Update 02_Price Elasticity Modelling.py

--- pages/02_Price Elasticity Modelling.py
+++ pages/02_Price Elasticity Modelling.py
@@ -79,7 +79,7 @@ def vis_elasticity(data):
     data = data[data['price_change'] != max_value]
 
     # Create a scatter plot
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10,10))
     ax.scatter(data['price_change'], data['sale_change'])
     ax.set_title('Scatter Plot of Price Change% vs Sale Change%')
     ax.set_xlabel('Price Change%')

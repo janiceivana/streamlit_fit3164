5787db4efffd592b87dff2f1392057c7792ff3a4
Janice Ivana <jiva0001@student.monash.edu>
2024-05-10 14:43:13 +1000

    Update 02_Price Elasticity Modelling.py

--- pages/02_Price Elasticity Modelling.py
+++ pages/02_Price Elasticity Modelling.py
@@ -7,11 +7,8 @@ import matplotlib.pylab as plt
 import streamlit as st
 from st_files_connection import FilesConnection
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('s3', type=FilesConnection)
-df = conn.read("fit3164-ds05/f1_opti.csv", input_format="csv", ttl=600)
-st.write(df)
+df = conn.read("fit3164-ds05/f1_opti.csv", input_format="csv")
 
 #######################
 # Page configuration

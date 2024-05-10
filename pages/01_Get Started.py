d49d68b29b3c8daa6e83a283b9a8e49e39e2f8a5
Janice Ivana <jiva0001@student.monash.edu>
2024-05-10 14:49:21 +1000

    test

---
+++ pages/01_Get Started.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Data Visualization App")
+
+session_state = st.session_state
+if 'dataframes' not in session_state:
+    session_state.dataframes = {}
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+if st.button("Load Data"):
+    for uploaded_file in uploaded_files:
+        file_name = uploaded_file.name.replace(".csv", "").replace(" ", "_")
+        session_state.dataframes[file_name] = pd.read_csv(uploaded_file)
+    st.success("Data loaded successfully!")
+
+

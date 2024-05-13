51d3e3f68865a8e09843c2ac9e1c56ad89d81a79
npha0026 <npha0026@student.monash.edu>
2024-05-13 19:19:27 +1000

    Update 04_Help.py

--- pages/04_Help.py
+++ pages/04_Help.py
@@ -6,8 +6,8 @@ st.markdown('#### Need Help?')
 st.image("help.jpeg")
 
 url = "https://github.com/janiceivana/streamlit_fit3164/blob/main/User_guide.pdf"
-st.write("Click here for user guide [link](%s)" % url)
-st.markdown("click here for user guide [link](%s)" % url)
+st.write("Click here for [User Guide](%s)" % url)
+
 
 st.write("Please contact our team to help you troubleshoot")
 st.write("Janice: jiva0001@student.monash.edu")
